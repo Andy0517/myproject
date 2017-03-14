4556fe47da4f7036f47d50dc5aad072d2407fd8c
Andy0517 <ad870517@gmail.com>
2017-03-14 15:56:04 +0800

    新增建構子

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -5,11 +5,11 @@ public class Car{
     String brand;
     int cc;
     String name;
-    String type;
-/*public Car(String brand,int cc,String name,String type){
+    //String type;
+public Car(String brand,String name,int cc){
    this.brand=brand;
    this.cc=cc;
    this.name=name;
-   this.type=type;
-}*/
+   //this.type=type;
+}
 }
--- src/oo/abstraction/Cartester.java
+++ src/oo/abstraction/Cartester.java
@@ -3,8 +3,9 @@ package oo.abstraction;
 public class Cartester {
 
 	public static void main(String[] args) {
-		Car c1 = new Car();
-
+		Car c1 = new Car("TOYOTA","ALTIS",1800);
+		Car c2 = new Car("HONDA","ACCORD", 2000);
+		Car c3 = new Car("TOYOTA","WISH", 2000);
 	}
 
 }
