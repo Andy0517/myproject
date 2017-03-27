80828eac0975a156397decaea5f97e239f93f981
Andy0517 <ad870517@gmail.com>
2017-03-27 21:17:12 +0800

    Update Shoestester.java

--- src/oo/abstraction/Shoestester.java
+++ src/oo/abstraction/Shoestester.java
@@ -3,7 +3,8 @@ package oo.abstraction;
 public class Shoestester {
 
 	public static void main(String[] args) {
-		Shoes s1 = new Shoes("NIKE",4000,"AIR","RUN"); 
+		Shoes s1 = new Shoes("NIKE",5200,"FLYKNIT RACER","run"); 
+		Shoes s2 = new Shoes("NIKE",4000,"AIR MAX","run"); 
 
 	}
 
