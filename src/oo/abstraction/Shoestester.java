0a36ba49d22be8e5eda6c652384db2fabe89f0de
Andy0517 <ad870517@gmail.com>
2017-03-27 21:24:16 +0800

    Update Shoestester.java

--- src/oo/abstraction/Shoestester.java
+++ src/oo/abstraction/Shoestester.java
@@ -6,6 +6,35 @@ public class Shoestester {
 		Shoes s1 = new Shoes("NIKE",5200,"FLYKNIT RACER","run"); 
 		Shoes s2 = new Shoes("NIKE",4000,"AIR MAX","run"); 
 
+		int[] n = new int[5];
+		System.out.println(n[2]);
+		
+		Shoes[] shoes= new Shoes[2];
+		System.out.println(shoes[0]);
+		
+		shoes[0] = s1;
+		shoes[1] = s2;
+		
+		
+		shoes[0].id = 001;
+		shoes[1].id = 002;
+		
+
+		
+		
+		
+		System.out.println(shoes[1].name);
+		int i;
+		for(i=0;i<2;i++){
+			System.out.println(shoes[i].name+"\t"+shoes[i].id);
+
+		}
+		System.out.println(i);
+		class AA {
+			
+		}
+		AA a = new AA();
+	}
 	}
 
-}
+
